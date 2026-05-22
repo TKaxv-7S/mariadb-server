42f038a925a42caeaa080a183b2f0976409adc97
Andrei Elkin <andrei.elkin@pp.inet.fi>
2026-05-22 19:13:44 +0300

    High priority MDL_BACKUP_BLOCK_COMMIT_RPL
    When the standard request of MDL_BACKUP_COMMIT is not granted to a worker
    it first searches for a possible grantee that is a parallel slave
    worker and if there exist one with greater commit order the request
    type is escalate to the highest which is re-submitted.
    
    To the changes in sql_base.cc,
    note there also exist a request of the highest (new) type at time of
    Xid_apply_log_event::do_record_gtid which is a committing phase of
    slave transaction when it is executing an implicit statement.
    It must be able to bypass any waiting lock by backup process even
    without yet knowing whether the transaction is going to be enrolled
    into a pending to commit slave group. When later it turns out
    not the case the strong MDL_STATEMENT requested lock would've been
    already relinquished.
    
    Tested with previous commit's  refined and extended BASE's
      rpl.parallel_backup, rpl.rpl_parallel_backup_worker_retry
    and new added
      rpl.rpl_parallel_backup_waits_worker_retry
    
    Todo:
    According to Svoj's review, this patch still needs
    removing the slave specific code from the MDL.
    That is changes in MDL_context::acquire_lock().
    
    Probably
     ha_commit_trans() would call first
        try_acquire_lock_impl()
     when it returns empty ticket a part of the added to acquire_lock
     Teammate-Aware Escalation Check would be invoked just to compute
     `has_teammate` and when that is true to call the whole BASE version
     of MDL_context::acquire_lock() having the highest type of request.

--- sql/rpl_rli.cc
+++ sql/rpl_rli.cc
@@ -2442,6 +2442,8 @@ void rpl_group_info::slave_close_thread_tables(THD *thd)
 /*
   The method computes weather the requestor's replicatated transaction
   can be grouped with the holder's one to share BACKUP MDL.
+  The requestor must be slave parallel worker or the driver thread
+  having special sub-id == zero.
 
   The method returns
     false
@@ -2463,18 +2465,19 @@ rpl_group_info::ignore_mdl_priority(const THD* holder, const THD* requestor)
   DBUG_ASSERT(requestor->system_thread == SYSTEM_THREAD_SLAVE_SQL);
   DBUG_ASSERT(requestor->rgi_slave->is_parallel_exec); // func is for parallel
 
-  rpl_group_info *rgi_hol= holder->rgi_slave;
-  rpl_group_info *rgi_req= requestor->rgi_slave;
+  rpl_group_info *rgi_holder= holder->rgi_slave;
+  rpl_group_info *rgi_requestor= requestor->rgi_slave;
 
-  if (unlikely(rgi_req->gtid_sub_id == 0))
+  if (unlikely(rgi_requestor->gtid_sub_id == 0))
     return false; ; // requestor is not a slave worker executing commit
 
   bool do_ignore=
     holder->system_thread == SYSTEM_THREAD_SLAVE_SQL &&
-    (rgi_hol->rli == rgi_req->rli &&
-     rgi_hol->current_gtid.domain_id == rgi_req->current_gtid.domain_id &&
-     rgi_hol->gtid_sub_id > 0 &&
-     rgi_hol->gtid_sub_id > rgi_req->gtid_sub_id);
+    (rgi_holder->rli == rgi_requestor->rli &&
+     rgi_holder->current_gtid.domain_id == rgi_requestor->current_gtid.domain_id
+     &&
+     rgi_holder->gtid_sub_id > 0 &&
+     rgi_holder->gtid_sub_id > rgi_requestor->gtid_sub_id);
 
   return do_ignore;
 }
