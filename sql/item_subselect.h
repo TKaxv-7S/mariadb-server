MariaDB/server

MDEV-30073 Wrong result on 2nd execution of PS for query with NOT EXISTS
This bug is consequence of a serious architectural flaw of the query
processing at the prepare stage. Context analysis of processed queries has
to be done at this stage. The important part of this analysis is resolution
of column references also known as name resolution. The items for column
references used in a query are created by the parser. They are allocated
in the statement memory because their life span is the same as the life
span of the executed query. During name resolution some of the items are
wrapped into objects belonging to classes derived from the Item_ref class.
In many cases we can't do without such wrappers. For example, the parser
does not differentiate between references to columns of base tables and
references to columns of views. However for a reference to a view column
we need a pointer to the translation item for this column. This pointer
is stored in a Item_direct_view_ref object that wraps the item for the
column reference. Before this patch the wrappers were allocated in
execution memory good only for one execution. That wasn't a problem when
the query was executed only once. Yet for the queries executed as prepared
statements or within a stored procedure it could lead to crashes or wrong
result sets at the second execution of the query. It could happen when
the wrapped item was substituted for something else by permanent
transformations during the optimization phase.

This patch allocates all the wrappers around items created for column
references in the statement memory at the first execution of PS or at the
first call of SP/SF.

sql/item_subselect.h
@@ -302,6 +302,7 @@ class Item_singlerow_subselect :public Item_subselect
 {
 protected:
   Item_cache *value, **row;
+  uchar strategy;
 public:
   Item_singlerow_subselect(THD *thd_arg, st_select_lex *select_lex);
   Item_singlerow_subselect(THD *thd_arg): Item_subselect(thd_arg), value(0), row (0)
@@ -346,6 +347,10 @@ public:
   st_select_lex* invalidate_and_restore_select_lex();
 
   Item* expr_cache_insert_transformer(THD *thd, uchar *unused);
+
+  bool test_set_strategy(uchar strategy_arg);
+
+  void set_strategy(uchar strategy_arg);
 
   friend class select_singlerow_subselect;
 };

TKaxv-7S/mariadb-server

sql/item_subselect.h
@@ -306,6 +306,7 @@ class Item_singlerow_subselect :public Item_subselect
 {
 protected:
   Item_cache *value, **row;
+  uint16 strategy;
 public:
   Item_singlerow_subselect(THD *thd_arg, st_select_lex *select_lex);
   Item_singlerow_subselect(THD *thd_arg): Item_subselect(thd_arg), value(0), row (0)
@@ -350,6 +351,10 @@ public:
   st_select_lex* invalidate_and_restore_select_lex();
 
   Item* expr_cache_insert_transformer(THD *thd, uchar *unused) override;
+
+  bool test_set_strategy(uchar strategy_arg);
+
+  void set_strategy(uchar strategy_arg);
 
   friend class select_singlerow_subselect;
 };